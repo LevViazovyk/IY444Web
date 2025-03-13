426a0a5d594fcca2a944193639a7f42d8000e689
Lev <T0462090@my.ntu.ac.uk>
2025-03-13 13:17:20 +0000

    hi

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const params = new URLSearchParams(window.location.search);
-    const recipeName = params.get("name");
 
-    const recipes = {
-        comingsoon1: { title: "Coming Soon...", description: "New recipe will be added soon!" },
-        comingsoon2: { title: "Coming Soon...", description: "New recipe will be added soon!" },
-        comingsoon3: { title: "Coming Soon...", description: "New recipe will be added soon!" },
-        comingsoon4: { title: "Coming Soon...", description: "New recipe will be added soon!" }
-    };
-
-    if (recipeName in recipes) {
-        document.getElementById("recipe-title").textContent = recipes[recipeName].title;
-        document.getElementById("recipe-description").textContent = recipes[recipeName].description;
-    } else {
-        document.querySelector(".recipe-container").innerHTML = "<h1>Recipe Not Found</h1>";
-    }
-});
 
 const toggleButton = document.querySelector('.toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
